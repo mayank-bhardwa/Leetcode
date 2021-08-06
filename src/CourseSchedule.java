5a3aced0fcbe792e709fca70c9de55e02270e0ab
Mayank Bhardwaj <mayank85277@gmail.com>
2021-08-06 22:08:37 +0530

    Finalised Course Schedule

--- src/CourseSchedule.java
+++ src/CourseSchedule.java
@@ -1,49 +1,58 @@
 import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Queue;
 
 public class CourseSchedule {
     public static void main(String[] args) {
-        int[][] prereq = { { 1, 4 }, { 2, 4 }, { 3, 1 }, { 3, 2 } };
-        int numCourses = 5;
+        int[][] prereq = { { 1, 0 }, { 0, 3 }, { 0, 2 }, { 3, 2 }, { 2, 5 }, { 4, 5 }, { 5, 6 }, { 2, 4 } };
+        int numCourses = 7;
 
         System.out.println(canFinish(numCourses, prereq));
     }
 
     private static boolean canFinish(int numCourses, int[][] prereq) {
-        Hashtable<Integer, List<Integer>> adj = new Hashtable<>();
-        for (int i = 0; i < prereq.length; i++) {
-            if (adj.containsKey(prereq[i][0]))
-                adj.get(prereq[i][0]).add(prereq[i][1]);
-            else {
-                adj.put(prereq[i][0], new ArrayList<>());
-                adj.get(prereq[i][0]).add(prereq[i][1]);
-            }
+
+        boolean[] visited = new boolean[numCourses];
+        boolean[] root = new boolean[numCourses];
+
+        HashMap<Integer, List<Integer>> lookup = new HashMap<>();
+
+        for (int[] pair : prereq) {
+            List<Integer> l = lookup.getOrDefault(pair[1], new ArrayList<>());
+            l.add(pair[0]);
+            lookup.put(pair[1], l);
         }
 
-        Queue<Integer> q = new LinkedList<>();
-
         for (int i = 0; i < numCourses; i++) {
-            if (adj.containsKey(i)) {
-                boolean[] visited = new boolean[numCourses];
-
-                q.clear();
-                q.offer(i);
-                while (!q.isEmpty()) {
-                    int t = q.poll();
-                    visited[t] = true;
-                    if (adj.containsKey(t))
-                        for (int k : adj.get(t)) {
-                            if (visited[k])
-                                return false;
-                            q.offer(k);
-                        }
-                }
+            if (!visited[i]) {
+                if (containCycle(i, lookup, visited, root))
+                    return false;
             }
         }
 
         return true;
+    }
+
+    private static boolean containCycle(int i, HashMap<Integer, List<Integer>> lookup, boolean[] visited,
+            boolean[] root) {
+        if (root[i])
+            return true;
+
+        if (visited[i])
+            return false;
+
+        root[i] = true;
+        visited[i] = true;
+
+        List<Integer> temp = lookup.getOrDefault(i, new ArrayList<>());
+        for (int j : temp) {
+            if (containCycle(j, lookup, visited, root)) {
+                return true;
+            }
+        }
+
+        root[i] = false;
+
+        return false;
     }
 }
